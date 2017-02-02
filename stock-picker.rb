eaa82c0ea4f09d8658bcba63a024f2d9ef88da24
Andrew <agstevens09@gmail.com>
2017-02-01 20:54:51 -0600

    Getting started on stock picker, need to work on finding lowest and highest days

--- stock-picker.rb
+++ stock-picker.rb
@@ -6,3 +6,20 @@ before you sell.
 Ex: stock_picker([17,3,6,9,15,8,6,1,10]) 
 returns [1,4]  # for a profit of $15 - $3 == $12
 =end
+
+def stock_picker(array)
+  buyAndSell = []
+  #find the lowest price
+  lowest = array.min
+  buyAndSell.push(array.index(lowest))
+
+  #find the highest price, after the lowest price
+  highest = array.max
+  buyAndSell.push(array.index(highest))
+
+  #edge cases are lowest day is last day, and higest is first day
+  p buyAndSell
+end
+
+stock_picker([17,3,6,9,15,8,6]) #should return [1, 4] 15-3 = $12 profit
+stock_picker([5, 2, 8, 9, 20, 7, 9])  #should return [1, 4] 20-2= $18 profit
